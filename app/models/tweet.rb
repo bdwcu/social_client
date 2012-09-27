1368d9c6c0e07c4ef022f49910159b4efef5814f
Matt Flesher <matt.flesher@gmail.com>
2012-09-27 12:04:16 -0600

    init commit

---
+++ app/models/tweet.rb
@@ -0,0 +1,7 @@
+class Tweet < ActiveRecord::Base
+  attr_accessible :content, :user_id
+
+  belongs_to :user
+
+  validates :content, :length => { :maximum => 140 }
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialClient::Application.config.secret_token = 'f2a123192f1fb44ac3bc3acaf60aa01528e079d9b1a59690985b30ec555ae79620162f0555644c828d05b8117a4a7192d00c515f895e80ab8db9938ab4041b94'
