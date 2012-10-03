26b08086bf488330b8f99594b5bce7e39f53d1b8
Nathan Beekman <nathan.beekman1@gmail.com>
2012-10-03 13:01:11 -0600

    initial commit of somewhat of a data model...

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '23c5f4c2502cd7c9cb4ff3c38b3dbb1637af2a189c6f23a088ba409b3ab1e96d831bcd18c59f217ddd9003874f7bdb264188a2631fdb1131511b578baabdfbab'
